61c70d1c9658e83783b0def16fa8cc7321a09733
jeyaparvathi38-coder <jeyaparvathi38@gmail.com>
2026-04-12 11:46:14 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,31 +1,42 @@
+
+Vignesh Vicky
+11:44 AM (0 minutes ago)
+to me
+
 import streamlit as st
 import requests
 
+#page config
 st.set_page_config(
     page_title="Sentiment Analysis",
-    page_icon="🎥",
-    layout="centered"
+    page_icon=" ",
+    layout="centered",      
 )
 
 st.title("Movie Review Sentiment Analysis")
-st.write("Enter a movie review and get instant sentiment prediction!")
+st.write("Enter a movie review and get instant sentiment prediction")
 
+#input box
 review = st.text_area("Enter your movie review here:", height=150)
 
+#predict button
 if st.button("Analyze Sentiment"):
     if review.strip() == "":
-        st.warning("Please enter a review first!")
+        st.warning("Please enter a review first")
     else:
         with st.spinner("Analyzing..."):
-            response = requests.post(
-                "https://movie-sentiment-analyzing-model-3.onrender.com/predict",
+            # Call your Flask backend
+            response = requests.post(a
+                "https://movie-review-sentiment-analysis-1-6pwi.onrender.com/predict",
                 json={"reviews": review}
             )
-
             result = response.json()
-            sentiment = result["prediction"]
 
-            if "pos" in sentiment.lower() or "neg" in sentiment.lower():
-                st.success(f"Sentiment: {sentiment}")
-            else:
-                st.error(f"Sentiment: {sentiment}")
+        #Display result
+        sentiment = result['prediction']
+        #confidence = result['confidence']
+
+        if"pos" in sentiment or "neg" in sentiment:
+            st.success(f"Sentiment: {sentiment}")
+        else:
+            st.error(f"Sentiment: {sentiment}")
